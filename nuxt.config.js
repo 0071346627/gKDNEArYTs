3816780342a5eb9df68b0354087c863ef72738ee
George <george@george.com>
2019-03-12 19:30:59 +0000

    add: nuxt config file

---
+++ nuxt.config.js
@@ -0,0 +1,62 @@
+import pkg from './package'
+
+const nuxtConfig = {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+  ** Loading bar colour
+  */
+  loading: { color: 'lime' },
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    '@nuxtjs/proxy',
+    'nuxt-svg-loader'
+  ],
+  router: {
+    linkExactActiveClass: 'is-active'
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false
+        }
+      }
+    },
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
+export default nuxtConfig
