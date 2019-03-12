c5ad548a10ccc1064b489741c3a53e6e35bf0e9a
George <george@george.com>
2019-03-12 19:38:57 +0000

    add: default layout

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-navbar :title="title" :links="links" />
+    <div style="min-height: calc(100vh-50px);">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import { LINKS as links, TITLE as title } from '~/content/navbar.json'
+
+export default {
+  data() {
+    return {
+      links,
+      title
+    }
+  },
+  middleware: 'weather'
+}
+</script>
